e3c878cf7677e81ac56ed406072e2c02c1d8d483
markanthony <mmaano1996@gmail.com>
2018-03-27 11:00:41 +0800

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('css')
+
+@endsection
+
+
+@section('content')
+<table>
+<th>Name</th>
+<td></td>
+</table>
+
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+<title>My Admin</title>
+<style>
+@yield('css')
+</style>
+</head>
+	
+	@yield('content')
+
+
+</html>
